85b3b12219586b3283b6451b51b7859f4ed89c8c
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 11:13:45 +0100

    switch /case days of the week exemple

---
+++ switch_case.js
@@ -0,0 +1,23 @@
+let today = tuseday
+switch (today) {
+    case 0 :
+        today =  "Sunday";
+    break;
+    case 1:
+        todayday = "Monday";
+        break;
+    case 2:
+        today = "Tuesday";
+        break;
+    case 3:
+        today = "Wednesday";
+        break;
+    case 4:
+        today = "Thursday";
+        break;
+    case 5:
+        today = "Friday";
+        break;
+    case  6:
+        today = "Saturday";
+}
