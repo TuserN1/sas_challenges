08970eb40425b92c02d36412ae995b7185572746
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 11:36:34 +0100

    add grade mark condition test

--- ifconditions.js
+++ ifconditions.js
@@ -12,9 +12,23 @@ else {
 let time = 10
 
 if (time < 10) {
-  greeting = "morning";
+  greeting = "Good morning";
 } else if (time < 20) {
   greeting = "Good day";
 } else {
   greeting = "Good evening";
 }
+
+//grade mark
+
+let note = 16
+
+
+if (note >= 15){
+  console.log("trés bien");
+
+}
+else if (note >= 10) {
+  console.log ("bien");
+}
+else  console.log("failed");
