081adceae52a938c991ead7f5ee5a88f7f3e7d75
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 10:11:50 +0100

    lent and const

--- let_const.js
+++ let_const.js
@@ -1,5 +1,5 @@
-let version = 280
+const version = 280
 let color = "blue"
-let description = "a wheeled motor vehicle designed primarily to run on roads, seat one to eight people, and transport passengers."
+const description = "a wheeled motor vehicle designed primarily to run on roads, seat one to eight people, and transport passengers."
 let work = true
 console.log("the car version is " + version +" its color is " + color );
