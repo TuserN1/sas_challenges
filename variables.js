52de03aea042ea9c5cebda826dcb67b4df55151a
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 10:53:10 +0100

    variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+// javascript does auto declaration of any variable it detects its type and alocate the propre memory size for it
+const x = 5
+const b = 2.2
+let a = "string"
+let z = true
+let h ;
+let n = null
+
+
+
+console.log(x);
+console.log(b);
+console.log(a);
+console.log(h);
+console.log(n);
