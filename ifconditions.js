2f0a8975887b61d4653868cab157fa9b6da0a751
TuserN1 <abakarimtahr@gmail.com>
2026-09-01 11:05:24 +0100

    if conditions with ifelse

---
+++ ifconditions.js
@@ -0,0 +1,20 @@
+let age = 18
+ 
+if (age >= 18) {
+    console.log("adult");
+
+    
+}
+else {
+    console.log("minor");
+}
+
+let time = 10
+
+if (time < 10) {
+  greeting = "morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
